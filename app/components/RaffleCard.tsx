9d5443b77f5717f629d5052008ddaf94bf0d68d7
aych <aychdev1@gmail.com>
2024-04-05 13:50:34 -0400

    updated raffle card format

--- app/components/RaffleCard.tsx
+++ app/components/RaffleCard.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'next/image';
+import dunkinLogo from '@/public/images/DunkinLogo.jpeg';
 
 interface Props {
   tokenOwned: string[];
@@ -8,17 +10,21 @@ const RaffleCard = ({ tokenOwned }: Props) => {
   return (
     <div className='border border-green-300 p-3 flex flex-col'>
       <div className='flex flex-col items-center justify-center'>
-        <h3>Tesla CyberTruck</h3>
+        <h3 className='text-center'>$100 Dunkin Donuts Gift Card</h3>
         <button className='border border-white p-2'>
-          {tokenOwned} NFTs Owned
+          {tokenOwned[0] ? 'Enter Raffle' : 'Not Eligble'}
         </button>
       </div>
       <div>
-        <div className='flex items-center justify-center'>IMAGE</div>
+        <Image
+          src={dunkinLogo}
+          alt='Dunkin Donuts Logo'
+          className='flex items-center justify-center rounded-full p-2'
+        />
       </div>
       <div className='flex justify-between'>
-        <p>How Many Entered</p>
-        <p>Time Left To Enter</p>
+        <p>100/100</p>
+        <p>2 Hours Left To Enter</p>
       </div>
     </div>
   );
