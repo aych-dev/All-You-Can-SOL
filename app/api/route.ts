0ea3be8cfe1d43f4dd310a3629f703165f452c04
aych <aychdev1@gmail.com>
2024-04-08 20:28:44 -0400

    added database for users

---
+++ app/api/route.ts
@@ -0,0 +1,16 @@
+import UserInfo from '../models/UserInfo';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const userInfoData = body.formData;
+    await UserInfo.create(userInfoData);
+    return NextResponse.json(
+      { message: 'User Info submitted' },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json({ message: 'Error POST', error }, { status: 500 });
+  }
+}
